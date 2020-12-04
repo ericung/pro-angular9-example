e1f1db70d0440f4eb5660bd1efc29487d56f71a1
Eric Ung <ungericw@tutanota.com>
2020-12-04 14:07:16 -0600

    12/4/2020 form model

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,12 +1,15 @@
 import { ApplicationRef, Component } from "@angular/core";
+import { NgForm, FormGroup } from "@angular/forms";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
+import { ProductFormGroup, ProductFormControl } from "./form.model";
 @Component({
     selector: "app",
     templateUrl: "template.html"
 })
 export class ProductComponent {
     model: Model = new Model();
+    formGroup: ProductFormGroup = new ProductFormGroup();
     getProduct(key: number): Product {
         return this.model.getProduct(key);
     }
@@ -19,5 +22,17 @@ export class ProductComponent {
     }
     addProduct(p: Product) {
         console.log("New Product: " + this.jsonProduct);
+    }
+    formSubmitted: boolean = false;
+    submitForm() {
+        Object.keys(this.formGroup.controls)
+            .forEach(c => this.newProduct[c] = this.formGroup.controls[c].value);
+        this.formSubmitted = true;
+        if (this.formGroup.valid) {
+            this.addProduct(this.newProduct);
+            this.newProduct = new Product();
+            this.formGroup.reset();
+            this.formSubmitted = false;
+        }
     }
 }
