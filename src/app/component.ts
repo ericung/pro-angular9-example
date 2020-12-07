643c9081073d223130d97ea6526c034af8f4861c
Eric Ung <ungericw@tutanota.com>
2020-12-07 16:49:50 -0600

    12/7/2020 understanding components

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,33 +1,15 @@
 import { ApplicationRef, Component } from "@angular/core";
-import { NgForm, FormGroup } from "@angular/forms";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
-import { ProductFormGroup, ProductFormControl } from "./form.model";
+import { ProductFormGroup } from "./form.model";
 @Component({
     selector: "app",
-    templateUrl: "template.html"
+    templateUrl: "template.html",
+    styles: ["/deep/ div { border: 2px black solid;  font-style:italic }"]
 })
 export class ProductComponent {
     model: Model = new Model();
-    formGroup: ProductFormGroup = new ProductFormGroup();
-    showTable: boolean = false;
-    darkColor: boolean = false;
-    
-    getProduct(key: number): Product {
-        return this.model.getProduct(key);
-    }
-    getProducts(): Product[] {
-        return this.model.getProducts();
-    }
-    newProduct: Product = new Product();
     addProduct(p: Product) {
         this.model.saveProduct(p);
-    }
-    deleteProduct(key: number) {
-        this.model.deleteProduct(key);
-    }
-    formSubmitted: boolean = false;
-    submitForm() {
-        this.addProduct(this.newProduct);
     }
 }
